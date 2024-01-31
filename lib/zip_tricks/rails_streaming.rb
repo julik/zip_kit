0a7959b4cf4598d5561e3f26caf4e6851999db2f
Julik Tarkhanov <me@julik.nl>
2024-01-31 11:14:11 +0100

    Fix Rails streaming yet again
    It turns out that there are a few things which were broken:
    
    * The addition of Rack::ETag middleware would kill streaming responses
    * The downstream HTTP proxy can request HTTP/1.0 and would then not support Transfer-Encoding: chunked
    
    So: if the upstream proxy wants HTTP/1.0 we at least indicate to the user that there
    is going to be buffering, so that they know where to look for configuration.
    
    To bypass ETag and ContentLength middleware we add some special sauce. And this feature should
    be tested against a real ActionController.

--- lib/zip_tricks/rails_streaming.rb
+++ lib/zip_tricks/rails_streaming.rb
@@ -5,16 +5,46 @@ module ZipTricks::RailsStreaming
   # Opens a {ZipTricks::Streamer} and yields it to the caller. The output of the streamer
   # gets automatically forwarded to the Rails response stream. When the output completes,
   # the Rails response stream is going to be closed automatically.
+  # @param filename[String] name of the file for the Content-Disposition header
+  # @param type[String] the content type (MIME type) of the archive being output
   # @param zip_streamer_options[Hash] options that will be passed to the Streamer.
   #     See {ZipTricks::Streamer#initialize} for the full list of options.
   # @yield [Streamer] the streamer that can be written to
   # @return [ZipTricks::OutputEnumerator] The output enumerator assigned to the response body
-  def zip_tricks_stream(**zip_streamer_options, &zip_streaming_blk)
-    # Set a reasonable content type
-    response.headers['Content-Type'] = 'application/zip'
-    # Make sure nginx buffering is suppressed - see https://github.com/WeTransfer/zip_tricks/issues/48
-    response.headers['X-Accel-Buffering'] = 'no'
-    response.sending_file = true
-    self.response_body = ZipTricks::OutputEnumerator.new(**zip_streamer_options, &zip_streaming_blk)
+  def zip_tricks_stream(filename: 'download.zip', type: 'application/zip', **zip_streamer_options, &zip_streaming_blk)
+    # The output enumerator yields chunks of bytes generated from ZipTricks. Instantiating it
+    # first will also validate the Streamer options.
+    chunk_yielder = ZipTricks::OutputEnumerator.new(**zip_streamer_options, &zip_streaming_blk)
+
+    # We want some common headers for file sending. Rails will also set
+    # self.sending_file = true for us when we call send_file_headers!
+    send_file_headers!(type: type, filename: filename)
+
+    # We need to ensure Rack::ETag does not suddenly start buffering us, see
+    # https://github.com/rack/rack/issues/1619#issuecomment-606315714
+    # Set this even when not streaming for consistency. The fact that there would be
+    # a weak ETag generated would mean that the middleware buffers, so we have tests for that.
+    headers["Last-Modified"] = Time.now.httpdate
+
+    # Check for the proxy configuration first. This is the first common misconfiguration which destroys streaming -
+    # since HTTP 1.0 does not support chunked responses we need to revert to buffering. The issue though is that
+    # this reversion happens silently and it is usually not clear at all why streaming does not work. So let's at
+    # the very least print it to the Rails log.
+    if request.get_header("HTTP_VERSION") == "HTTP/1.0"
+      logger.warn { "The downstream HTTP proxy/LB insists on HTTP/1.0 protocol, ZIP response will be buffered." } if logger
+      self.response_body = chunk_yielder
+    else
+      # Disable buffering for both nginx and Google Load Balancer, see
+      # https://cloud.google.com/appengine/docs/flexible/how-requests-are-handled?tab=python#x-accel-buffering
+      response.headers["X-Accel-Buffering"] = "no"
+
+      # Make sure Rack::ContentLength does not try to compute a content length,
+      # and remove the one already set
+      headers["Transfer-Encoding"] = "chunked"
+      headers.delete("Content-Length")
+
+      # Assign the chunked body to response_body, Rack will pick it up from there
+      self.response_body = Rack::Chunked::Body.new(chunk_yielder)
+    end
   end
 end
