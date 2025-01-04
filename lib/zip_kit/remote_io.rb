9f86d72fe966f944147d27c128e292f484eeaeee
Julik Tarkhanov <me@julik.nl>
2025-01-04 22:16:28 +0000

    Set SSL for HTTPS connections in RemoteUncap
    shamelessly lifted from https://github.com/TrevorHinesley/zip_kit/commit/f612740621deae4a6ab73e395506a73a877ddade#diff-eb932899666ac92d3f1d8e43468916b9990e3560024f70cc5c86a2dc9edd8476R94

--- lib/zip_kit/remote_io.rb
+++ lib/zip_kit/remote_io.rb
@@ -74,7 +74,7 @@ class ZipKit::RemoteIO
   # @param range[Range] the HTTP range of data to fetch from remote
   # @return [String] the response body of the ranged request
   def request_range(range)
-    http = Net::HTTP.start(@uri.hostname, @uri.port)
+    http = Net::HTTP.start(@uri.hostname, @uri.port, use_ssl: @uri.scheme == "https")
     request = Net::HTTP::Get.new(@uri)
     request.range = range
     response = http.request(request)
@@ -91,7 +91,7 @@ class ZipKit::RemoteIO
   #
   # @return [Integer] the size of the remote resource, parsed either from Content-Length or Content-Range header
   def request_object_size
-    http = Net::HTTP.start(@uri.hostname, @uri.port)
+    http = Net::HTTP.start(@uri.hostname, @uri.port, use_ssl: @uri.scheme == "https")
     request = Net::HTTP::Get.new(@uri)
     request.range = 0..0
     response = http.request(request)
